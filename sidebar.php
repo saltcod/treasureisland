5b83468634925eab87eeafae419b1d6ae6eef3a0
Terry Sutton <saltcod@gmail.com>
2012-09-11 12:02:09 -0230

    Remove search widget from sidebar

--- sidebar.php
+++ sidebar.php
@@ -19,25 +19,7 @@
 
 		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
-			<aside id="search" class="widget widget_search">
-				<?php get_search_form(); ?>
-			</aside>
-
-			<aside id="archives" class="widget">
-				<h1 class="widget-title"><?php _e( 'Archives', 'toolbox' ); ?></h1>
-				<ul>
-					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-				</ul>
-			</aside>
-
-			<aside id="meta" class="widget">
-				<h1 class="widget-title"><?php _e( 'Meta', 'toolbox' ); ?></h1>
-				<ul>
-					<?php wp_register(); ?>
-					<aside><?php wp_loginout(); ?></aside>
-					<?php wp_meta(); ?>
-				</ul>
-			</aside>
+			  
 
 		<?php endif; // end sidebar widget area ?>
 
